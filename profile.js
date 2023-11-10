fa194e559c94b158a7ce9657a4bab0ebd16d8d52
trungkien130 <kienlee14@gmail.com>
2023-11-11 00:43:20 +0700

    update slide

--- profile.js
+++ profile.js
@@ -12,4 +12,19 @@ $(function () {
   sigup = function () {
     $("#sigupModal").show()
   }
-})
+  const slidesContainer = $(".slides");
+  const slides = $(".slide");
+let currentIndex = 0;
+  function nextSlide() {
+      currentIndex = (currentIndex + 1) % slides.length;
+      updateSlider();
+  }
+
+  function updateSlider() {
+      const translateValue = -currentIndex * 100 + "%";
+      slidesContainer.css("transform", "translateX(" + translateValue + ")");
+  }
+
+  // Auto slide every 3 seconds (adjust as needed)
+  setInterval(nextSlide, 3000);
+});
--- webbansach.php
+++ webbansach.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="profile.js"></script>
-    <link rel="stylesheet" href="All.css">
+    <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="fontawesome-free-6.4.2-web/css/all.css">
     <title>Vivabook.com</title>
 </head>
@@ -71,15 +71,23 @@
             </li>
         </ul>
     </div>
-    <div class="slice">
-        <div class="slider">
-            <div class="slide" id="slide-1"><img class="imgSlide" src="img/372419_loi-than-va-nhan-vien-van-phong-tap-1.jpg" alt=""></div>
-            <div class="slide" id="slide-2"><img class="imgSlide" src="img/371474_loi-than-va-nhan-vien-van-phong-tap-2.jpg" alt=""></div>
-            <div class="slide" id="slide-3"><img class="imgSlide" src="img/372415_loi-than-va-nhan-vien-van-phong-tap-3.jpg" alt=""></div>
-            <div class="slide" id="slide-4"><img class="imgSlide" src="img/372418_loi-than-va-nhan-vien-van-phong-tap-4.jpg" alt=""></div>
-            <div class="slide" id="slide-5"><img class="imgSlide" src="img/372416_loi-than-va-nhan-vien-van-phong-tap-5.jpg" alt=""></div>
+    <div id="slideshow">
+
+        <div class="slide-wrapper">
+            <div class="slide" id="slide-1"><img class="imgSlide" src="img/372419_loi-than-va-nhan-vien-van-phong-tap-1.jpg">
+            <div class="slide-content">
+                    <h1>Lôi Thần Và Nhân Viên Văn Phòng</h1>
+                    <p>Tác giả: Rena</p>
+                    <p>Nhà xuất bản: Nxb Hà Nội</p>
+                    <p>Nhà phát hành: AZ books</p>
+                    <span>Anh chàng gay Omura là một nhân viên văn phòng bình thường, cuộc sống mỗi ngày trôi qua một cách mệt mỏi và nhàm chán...</span>
+                </div>
+            </div>
+            <div class="slide" id="slide-2"><img class="imgSlide" src="img/371474_loi-than-va-nhan-vien-van-phong-tap-2.jpg"></div>
+            <div class="slide" id="slide-3"><img class="imgSlide" src="img/372415_loi-than-va-nhan-vien-van-phong-tap-3.jpg"></div>
+            <div class="slide" id="slide-4"><img class="imgSlide" src="img/372418_loi-than-va-nhan-vien-van-phong-tap-4.jpg"></div>
+            <div class="slide" id="slide-5"><img class="imgSlide" src="img/372416_loi-than-va-nhan-vien-van-phong-tap-5.jpg"></div>
         </div>
-    </div>
 
 </body>
 
